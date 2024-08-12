171254703d98438ba2dc21fb7f60ebccf8ec3ef4
vishal-lakhe1 <vishallakhe445@gmail.com>
2024-08-13 05:05:08 +0530

    first commit

---
+++ testCases/conftest.py
@@ -0,0 +1,39 @@
+import pytest as pytest
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def setup(request):
+    browser = request.config.getoption("--browser")
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "edge":
+        driver = webdriver.Edge()
+    else:
+        driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://bankapp.credence.in/")
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(params=[
+
+    ("Admin", "pass"),
+    ("Tushar", "pass"),
+    ("Admin420", "fail"),
+    ("demo2", "pass"),
+    ("demo", "fail")
+])
+def getDataForSearchUser(request):
+    return request.param
